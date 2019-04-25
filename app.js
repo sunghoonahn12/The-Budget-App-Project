23cb1888dddd5a7a836035e0720dc350a9fe4d13
Sunghoon Ahn <ahnsunghoon0215@gmail.com>
2019-04-25 00:15:58 -0400

    Implementing module patter

---
+++ app.js
@@ -0,0 +1,27 @@
+// BUDGET CONTROLLER
+var budgetController = (function() {
+    
+  
+    
+})();
+
+
+
+
+// UI CONTROLLER
+var UIController = (function() {
+    
+    
+    
+})();
+
+
+
+
+// GLOBAL APP CONTROLLER
+var controller = (function(budgetCtrl, UICtrl) {
+    
+    
+    
+})(budgetController, UIController);
+
