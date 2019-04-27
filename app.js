5f329ecf23366f3ce222b002e30bbddcde01d23a
Sunghoon Ahn <ahnsunghoon0215@gmail.com>
2019-04-27 14:47:05 -0400

    Built first event handler

--- app.js
+++ app.js
@@ -1,6 +1,12 @@
 // BUDGET CONTROLLER
 var budgetController = (function() {
     
+    var Expense = function(id, description, value) {
+        this.id = id;
+        this.description = description;
+        this.value = value;
+        this.percentage = -1;
+    };
   
     
 })();
@@ -21,6 +27,21 @@ var UIController = (function() {
 // GLOBAL APP CONTROLLER
 var controller = (function(budgetCtrl, UICtrl) {
     
+    var setupEventListeners = function() {
+        var DOM = UICtrl.getDOMstrings();
+        
+        document.querySelector(DOM.inputBtn).addEventListener('click', ctrlAddItem);
+
+        document.addEventListener('keypress', function(event) {
+            if (event.keyCode === 13 || event.which === 13) {
+                ctrlAddItem();
+            }
+        });
+        
+        document.querySelector(DOM.container).addEventListener('click', ctrlDeleteItem);
+        
+        document.querySelector(DOM.inputType).addEventListener('change', UICtrl.changedType);        
+    };
     
     
 })(budgetController, UIController);
