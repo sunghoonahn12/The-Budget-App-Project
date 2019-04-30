ffb0168b4d7b3121a0ca45cb5b32ead619c63d04
Sunghoon Ahn <ahnsunghoon0215@gmail.com>
2019-04-29 21:56:14 -0400

    Clear inputs

--- app.js
+++ app.js
@@ -98,6 +98,20 @@ var UIController = (function() {
             // Insert the HTML into the DOM
             document.querySelector(element).insertAdjacentHTML('beforeend', newHtml);
         },
+        
+        clearFields: function() {
+            var fields, fieldsArr;
+            
+            fields = document.querySelectorAll(DOMstrings.inputDescription + ', ' + DOMstrings.inputValue);
+            
+            fieldsArr = Array.prototype.slice.call(fields);
+            
+            fieldsArr.forEach(function(current, index, array) {
+                current.value = "";
+            });
+            
+            fieldsArr[0].focus();
+        },
     }
     
 })();
