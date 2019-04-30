799a32f60aee386c24b287aa2eafae2e8c3cbc04
Sunghoon Ahn <ahnsunghoon0215@gmail.com>
2019-04-29 22:02:04 -0400

    calculate percentage

--- app.js
+++ app.js
@@ -8,6 +8,19 @@ var budgetController = (function() {
         this.percentage = -1;
     };
   
+    Expense.prototype.calcPercentage = function(totalIncome) {
+        if (totalIncome > 0) {
+            this.percentage = Math.round((this.value / totalIncome) * 100);
+        } else {
+            this.percentage = -1;
+        }
+    };
+    
+    
+    Expense.prototype.getPercentage = function() {
+        return this.percentage;
+    };
+
     var Income = function(id, description, value) {
         this.id = id;
         this.description = description;
